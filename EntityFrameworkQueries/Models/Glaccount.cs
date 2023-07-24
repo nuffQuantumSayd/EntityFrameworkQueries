42039a6ac2c628c24be6b6034c1072e8be633cae
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-07-24 11:03:46 -0700

    Reverse engineered AP database to EF Core model

---
+++ EntityFrameworkQueries/Models/Glaccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityFrameworkQueries.Models;
+
+public partial class Glaccount
+{
+    public int AccountNo { get; set; }
+
+    public string AccountDescription { get; set; } = null!;
+
+    public virtual ICollection<InvoiceLineItem> InvoiceLineItems { get; set; } = new List<InvoiceLineItem>();
+
+    public virtual ICollection<Vendor> Vendors { get; set; } = new List<Vendor>();
+}
---
+++ EntityFrameworkQueries/Models/Term.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityFrameworkQueries.Models;
+
+public partial class Term
+{
+    public int TermsId { get; set; }
+
+    public string TermsDescription { get; set; } = null!;
+
+    public short TermsDueDays { get; set; }
+
+    public virtual ICollection<Invoice> Invoices { get; set; } = new List<Invoice>();
+
+    public virtual ICollection<Vendor> Vendors { get; set; } = new List<Vendor>();
+}
