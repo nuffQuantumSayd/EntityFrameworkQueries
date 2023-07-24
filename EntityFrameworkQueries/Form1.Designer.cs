4310f4dee4aa5a854dbe13d85c881b337d88397e
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-07-24 11:51:21 -0700

    Added query to demonstrate Where and OrderBy

--- EntityFrameworkQueries/Form1.Designer.cs
+++ EntityFrameworkQueries/Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnSelectAllVendors = new Button();
+            btnAllCAVendors = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // btnSelectAllVendors
+            // 
+            btnSelectAllVendors.Location = new Point(41, 84);
+            btnSelectAllVendors.Name = "btnSelectAllVendors";
+            btnSelectAllVendors.Size = new Size(143, 23);
+            btnSelectAllVendors.TabIndex = 0;
+            btnSelectAllVendors.Text = "Select * From Vendors";
+            btnSelectAllVendors.UseVisualStyleBackColor = true;
+            btnSelectAllVendors.Click += btnSelectAllVendors_Click;
+            // 
+            // btnAllCAVendors
+            // 
+            btnAllCAVendors.Location = new Point(41, 165);
+            btnAllCAVendors.Name = "btnAllCAVendors";
+            btnAllCAVendors.Size = new Size(414, 23);
+            btnAllCAVendors.TabIndex = 1;
+            btnAllCAVendors.Text = "Select * From Vendors Where VendorState = CA OrderBy VendorName";
+            btnAllCAVendors.UseVisualStyleBackColor = true;
+            btnAllCAVendors.Click += btnAllCAVendors_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(41, 245);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(btnAllCAVendors);
+            Controls.Add(btnSelectAllVendors);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btnSelectAllVendors;
+        private Button btnAllCAVendors;
+        private Button button3;
     }
 }
--- EntityFrameworkQueries/Form1.cs
+++ EntityFrameworkQueries/Form1.cs
@@ -1,3 +1,7 @@
+using EntityFrameworkQueries.Data;
+using EntityFrameworkQueries.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace EntityFrameworkQueries
 {
     public partial class Form1 : Form
@@ -5,6 +9,33 @@ namespace EntityFrameworkQueries
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnSelectAllVendors_Click(object sender, EventArgs e)
+        {
+            using ApContext dbContext = new ApContext();
+
+            // LINQ (Language Integrated Query) method syntax
+            List<Vendor> vendorList = dbContext.Vendors.ToList();
+
+            // LINQ query syntax
+            List<Vendor> vendorList2 = (from v in dbContext.Vendors
+                                        select v).ToList();
+        }
+
+        private void btnAllCAVendors_Click(object sender, EventArgs e)
+        {
+            using ApContext dbContext = new ApContext();
+
+            List<Vendor> vendorList = dbContext.Vendors
+                                        .Where(v => v.VendorState == "CA")
+                                        .OrderBy(v => v.VendorName)
+                                        .ToList();
+
+            List<Vendor> vendorList2 = (from v in dbContext.Vendors
+                                        where v.VendorState == "CA"
+                                        orderby v.VendorName
+                                        select v).ToList();
         }
     }
 }
